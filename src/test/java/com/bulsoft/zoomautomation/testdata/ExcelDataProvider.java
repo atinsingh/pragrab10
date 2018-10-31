20eff4161104b3fafe045b10823014992eddf1f3
Atin Singh <P@ssw0rd>
2018-10-30 22:11:21 -0400

    Change configuration for the data

--- src/test/java/com/bulsoft/zoomautomation/testdata/ExcelDataProvider.java
+++ src/test/java/com/bulsoft/zoomautomation/testdata/ExcelDataProvider.java
@@ -1,4 +1,20 @@
 package com.bulsoft.zoomautomation.testdata;
 
-public class ExcelDataProvider {
+import com.bulsoft.zoomautomation.configuration.DriverConfig;
+
+public class ExcelDataProvider{
+
+    private String fileName = DriverConfig.getPropertyValue("testdata.excel.path");
+
+    // Put your excel file in location
+    // Read the file as inpuStream
+    // pass it XFFWorkWook
+    // Sheet
+    // ROws ==> Using Row Iterator
+    // Row  -- >   You need get Cell Iterator
+    // Cell Iterator -- you need to get Cell
+    // Read cell value either of type
+    //  Numeric/String/Boolean/Date
+    // return in the format of
+    // List.iterator<Object[]>ls
 }
